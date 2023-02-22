e18103d816f1a3b50198832749fbd0e1f86d18d9
ErickDanielAvilaAvila <124694642+ErickDanielAvilaAvila@users.noreply.github.com>
2023-02-22 12:01:51 -0600

    Add files via upload

---
+++ EVA1_8_LLAMADAFUNCION/src/eva1_8_llamadafuncion/EVA1_8_LLAMADAFUNCION.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_8_llamadafuncion;
+
+/**
+ *
+ * @author avila
+ */
+public class EVA1_8_LLAMADAFUNCION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("INICIE Main()");
+        
+        System.out.println("TERMINA main()");
+    }
+    public static void  A(){
+        System.out.println("INICIA A()");
+ 
+        System.out.println("TERMINA A()");
+        
+    }
+    public static void B(){
+        System.out.println("INICIA B()");
+ 
+        System.out.println("TERMINA B()");
+        
+    }
+    
+}
