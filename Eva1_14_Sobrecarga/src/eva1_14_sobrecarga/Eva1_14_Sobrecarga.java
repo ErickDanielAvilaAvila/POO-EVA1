4497d9bfb28b2f0506efc833a6031391c33fa922
ErickDanielAvilaAvila <124694642+ErickDanielAvilaAvila@users.noreply.github.com>
2023-02-28 21:25:52 -0600

    Add files via upload

---
+++ Eva1_14_Sobrecarga/src/eva1_14_sobrecarga/Eva1_14_Sobrecarga.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_14_sobrecarga;
+
+/**
+ *
+ * @author avila
+ */
+public class Eva1_14_Sobrecarga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*Diferentes println: son 10 diferentes 
+        metodos, para imprimir diferentes tips de datos*/
+        /*para crear sobrecarga de metodos:
+        la firma de metodos debe de ser unica*/
+        System.out.println("Hola Mundo");
+        System.out.println("100");
+        System.out.println(true);
+        System.out.println(args);
+    }
+    
+}
